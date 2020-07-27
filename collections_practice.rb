1721ba7e554be2009af57ac8507b51bee85630ee
Joel Croitoru <jcroitoru@gmail.com>
2020-07-27 13:49:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
-
+def sort_array_asc(integers)
+  integers.sort
+end
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+def sort_array_char_count(array)
+  array.sort do |a,  b|
+    a.length <=> b.length
+  end
+end
+def swap_elements(array)
+  array.sort
+end
